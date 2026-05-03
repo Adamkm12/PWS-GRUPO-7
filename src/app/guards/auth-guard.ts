db4f53675786fc4bf9b88cf9e2e86e41279e082a
Pegasos2005 <cristiantomasvegaappelqvist@gmail.com>
2026-05-03 19:21:46 +0100

    Rama creada

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,4 +1,3 @@
-// src/app/components/guards/auth-guard.ts
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Auth, user } from '@angular/fire/auth';
